66834feb11808c65c16a5061d0d0c1665fac5315
michlimek <46439928+michlimek@users.noreply.github.com>
2025-07-13 15:42:54 +0200

    Update __init__.py
    Repair launching problem in HA 2025.7

--- custom_components/allegro/__init__.py
+++ custom_components/allegro/__init__.py
@@ -55,12 +55,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
     for platform in PLATFORMS:
-        if entry.options.get(platform, True):
-            coordinator.platforms.append(platform)
-            hass.async_add_job(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
-
+     if entry.options.get(platform, True):
+        coordinator.platforms.append(platform)
+        await hass.config_entries.async_forward_entry_setups(entry, [platform])
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
 
